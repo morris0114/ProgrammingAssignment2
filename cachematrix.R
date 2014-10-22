c8d2da3cb1aaa983382fade84ab51f14da627010
morris0114 <morris.ms.chen@gmail.com>
2014-10-22 23:25:51 +0800

    add comments
    Add comments for each functions nested in function "makeCacheMatrix"

--- cachematrix.R
+++ cachematrix.R
@@ -15,16 +15,55 @@
 ## a list of functions to operate input matrix, x.
 ###############################################################################
 makeCacheMatrix <- function(x = matrix()) {
+  # initialize local variable "inv" as NULL.
   inv<-NULL
+
+  #######################################################################
+  ## Description :
+  ## Function "set" is used to set matrix y into variable, x
+  ## 
+  ## Input :
+  ## y - a matrix
+  #######################################################################
   set<-function(y) {
+    # check whether the input matrix, y, is same as x or not.
     if(!all(x == y)) {
+    # y is not the same as x and set x as y
       x <<- y
+    # clear value of variable "inv"
       inv <<-NULL      
     }
   }
+
+  #######################################################################
+  ## Description :
+  ## Function "get" is used to get matrix "x"
+  ## 
+  ## Return :
+  ## a matrix
+  #######################################################################
   get<-function()x
-  setinv<-function(inverse) inv<<-inverse
+
+  #######################################################################
+  ## Description :
+  ## Function "setinv" is used to set matrix "inverse" into
+  ## variable "inv"
+  ## 
+  ## Input :
+  ## a matrix
+  #######################################################################
+  setinv<-function(inverse) inv<<-inverse # set inversed matrix
+
+  #######################################################################
+  ## Description :
+  ## Function "getinv" is used to get value of inverse matrix "inv"
+  ## 
+  ## Return :
+  ## an inverse matrix
+  #######################################################################
   getinv<-function() inv
+
+  # return a list of functions defined in function "makeCacheMatrix"
   list(set=set, get=get, setinv=setinv, getinv=getinv)  
 }
 
@@ -40,7 +79,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Input :
 ## x - a list of functions return from function "makeCacheMatrix"
 ## Return :
-## an inverse matrix
+## an inversed matrix
 ###############################################################################
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
